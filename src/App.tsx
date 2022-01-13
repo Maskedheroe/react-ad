ff00eed876a69d37eff75d22fbfbf8e3d1c9978d
方啸宇 <maskedfang@gmail.com>
2022-01-13 11:22:22 +0800

    首次commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useMemo, useState } from "react"
+import styles from './style.module.scss'
+
+function App() {
+  const [state, setState] = useState({ name: "1" })
+  return (
+    <div className="App">
+      123
+    </div>
+  )
+}
+
+export default App
+    
